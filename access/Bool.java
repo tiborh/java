76cc9b12765b631fe5c2aacd84bf29f78a675a5d
tibor harcsa <tibor@harcsa.net>
2016-11-14 02:34:01 +0100

    1. adding cloning and equalsTo methods, 2. removing createTrue and createFalse methods

--- access/Bool.java
+++ access/Bool.java
@@ -29,6 +29,16 @@ public class Bool
     }
 
     /**
+     * Initial state can be set with another Bool object
+     * 
+     * @param another Bool object
+     */
+    public Bool(Bool other)
+    {
+        state = other.isTrue();
+    }
+    
+    /**
      * Initial state can be set with an integer
      * 0: false
      * other values: true
@@ -39,21 +49,7 @@ public class Bool
     {
         state = initState == 0 ? false : true;
     }
-        
-    /**
-     * Create and return a new Bool object
-     * 
-     * @return new Bool object with "true" initial state
-     */
-    public Bool createTrue () { return(new Bool(true)); }
 
-    /**
-     * Create and return a new Bool object
-     * 
-     * @return new Bool object with "false" initial state
-     */
-    public Bool createFalse () { return(new Bool()); }
-    
     /**
      * The "and" logical operation between two Bool objects
      * 
@@ -64,7 +60,7 @@ public class Bool
     {
         return(new Bool(state && other.state));
     }
-    
+
     /**
      * The "or" logical operation between two Bool objects
      * 
@@ -75,7 +71,7 @@ public class Bool
     {
         return(new Bool(state || other.state));
     }
-    
+
     /**
      * The "not" logical operation between two Bool objects
      * 
@@ -86,15 +82,59 @@ public class Bool
     {
         return(new Bool(!state));
     }
+
+    /**
+     * Get information about the state.
+     * 
+     * @return boolean value reflecting the state of the Bool object
+     */
+    public Boolean isTrue() { return state == true; }
+
+    public Bool clone() { return new Bool(this); }
     
+    /**
+     * Get the state as a string.
+     * 
+     * @return state as a string
+     */
     public String toString() { return("" + state); }
-    
+
+    public boolean equals(Object obj)
+    {
+        if (this == null && obj == null)
+        {
+            return true;
+        }
+        if (obj == null || this == null)
+        {
+            return false;
+        }
+        if (!Bool.class.isAssignableFrom(obj.getClass()))
+        {
+            return false;
+        }
+        if (this == obj)
+        {
+            return true;
+        }
+        final Bool other = (Bool) obj;
+        if (this.isTrue() != other.isTrue()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * A few simple operations to show how the object can be used.
+     * 
+     * @param argv arguments are not used
+     */
     public static void main (String[] argv)
     {
         Bool b1 = new Bool();
-        Bool b2 = b1.createTrue();
+        Bool b2 = new Bool(true);
         Bool b3 = b1.not();
-        
+
         System.out.println("b1: " + b1);
         System.out.println("b2: " + b2);
         System.out.println("b3: " + b3);
