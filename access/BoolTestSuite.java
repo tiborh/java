3a020efed6c538bbbdf633421eac7cb92b6d9a44
tibor harcsa <tibor@harcsa.net>
2016-11-15 02:34:54 +0100

    further ADT exercises

--- access/BoolTestSuite.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-//Works if run by a runner.
-@RunWith(Suite.class)
-
-@Suite.SuiteClasses( { BoolTest.class})
-
-public class BoolTestSuite { }
