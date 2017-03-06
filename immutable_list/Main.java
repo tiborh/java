7dc0dc0c8cee84208881bc4ec60ab685129a5dd6
Tibor Harcsa <silurust@gmail.com>
2017-03-06 02:56:12 +0100

    timing tests

--- immutable_list/Main.java
+++ immutable_list/Main.java
@@ -7,6 +7,20 @@
  */
 public class Main
 {
+    private static long test_timing(int slength) {
+        ImList<Integer> ab = ImList.empty();
+        for (int i = 0; i < slength; ++i) {
+            ab = ab.cons(i);
+        }
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < slength; ++i) {
+            ab.size();
+            ab.get(i);
+        }
+        long endTime = System.currentTimeMillis();
+        return(endTime - startTime);
+    }
+
     public static void main(String[] argv) {
         ImList<Integer> nil = ImList.empty();   // []
         assert(nil.isEmpty());
@@ -36,19 +50,18 @@ public class Main
         assert(y.contains(4));
         assert(y.contains(2));
         assert(1 == y.get(1));
-        
+
         ImList<Integer> z = x.append(y);
         System.out.println("x.append(y) == " + z);
         assert(6 == z.size());
         assert(4 == z.get(3));
         assert(2 == z.get(5));
-        
+
         ImList<Integer> aa = z.reverse();
         System.out.println("z.reverse() == " + aa);
         assert(6 == aa.size());
         assert(2 == aa.get(0));
         assert(0 == aa.get(5));
-        
 
         ImList<Object> airports = 
             ImList.empty().cons("SFO").cons("IAD").cons("BOS");
@@ -60,6 +73,17 @@ public class Main
         System.out.println(airports);
         System.out.println(airports.first());
         System.out.println(airports.rest().rest());
-    }
 
+        int num_of_tries = 100;
+        int list_size = 2000;
+        long sum_of_durs = 0;
+        System.out.println("Speed test:");
+        for (int i = 0; i < num_of_tries; ++i) {
+            sum_of_durs+=test_timing(list_size);
+        }
+        System.out.println("list size: " + list_size);
+        System.out.println("number of runs: " + num_of_tries);
+        System.out.println("average execution duration: " + sum_of_durs/num_of_tries + " (milliseconds)");
+    }
 }
+
